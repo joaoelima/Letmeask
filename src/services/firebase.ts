53049c779ef45dbd9853c96bc42917f571f2bcf0
joaoedulima <106280952+joaoelima@users.noreply.github.com>
2022-06-03 20:33:45 -0300

    Atualização firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAH-TRefIWU87xv7l6nE2DFVmJA7Uz2Oco",
-    authDomain: "letmeask-1f4b3.firebaseapp.com",
-    databaseURL: "https://letmeask-1f4b3-default-rtdb.firebaseio.com",
-    projectId: "letmeask-1f4b3",
-    storageBucket: "letmeask-1f4b3.appspot.com",
-    messagingSenderId: "190527693418",
-    appId: "1:190527693418:web:2c9ae967af26d4847d6013"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
   firebase.initializeApp(firebaseConfig);
