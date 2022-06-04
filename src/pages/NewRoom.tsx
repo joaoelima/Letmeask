285534f5005c8c9ff6b8ee14ab051c57da9a7918
joaoedulima <106280952+joaoelima@users.noreply.github.com>
2022-06-04 18:33:33 -0300

    Att

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -7,7 +7,7 @@ import '../styles/auth.scss';
 
 
 
-export function Home(){
+export function NewRoom(){
     return(
         <div id="page-auth">
             <aside>
@@ -18,20 +18,19 @@ export function Home(){
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask"/>
-                    <button className="create-room">
-                        <img src={googleIconImg} alt="Logo do google"/>
-                        Crie sua sala com o Google
-                    </button>
-                    <div className="separator">Ou entre em uma sala</div>
+                    <h2>Criar uma nova sala</h2>       
                     <form>
                         <input
                             type="text"
-                            placeholder="Digite o código da sala"
+                            placeholder="Nome da sala"
                         />
                         <Button type="submit">
-                            Entrar na sala
-                        </Button>
+                            Criar sala
+                        </Button>                       
                     </form>
+                    <p>
+                      Quer entrar em uma sala existente? <a href="#"> clique aqui </a> 
+                    </p>
                 </div>
             </main>
         </div>
