ec2277c5ea2ca8ce4fa5d55e01140d2f9bf1acaf
joaoedulima <106280952+joaoelima@users.noreply.github.com>
2022-06-11 14:48:01 -0300

    Incremento na aplicação para criação de salas.

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
-
+import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext';
+
 
 //Função onde são feitas as chamadas de tela.
 function App() {
@@ -13,7 +14,8 @@ function App() {
     <AuthContextProvider>       
        <Routes>        
         <Route path="/" element={<Home />} />
-        <Route path="/rooms/new" element={<NewRoom />} />       
+        <Route path="/rooms/new" element={<NewRoom />} />
+        <Route path="/rooms/:id" element={<Room />} />        
        </Routes>
     </AuthContextProvider>             
     </BrowserRouter>
